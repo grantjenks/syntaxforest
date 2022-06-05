72c419dbc74279bdca117b56e5afa24815acf0d0
Grant Jenks <grant.jenks@gmail.com>
2022-06-05 15:58:37 -0700

    Add static root

--- syntaxforest/settings.py
+++ syntaxforest/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

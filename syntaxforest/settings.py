020b073e0467be5226edd0f98b82e0c53f5a15ac
Grant Jenks <grant.jenks@gmail.com>
2022-06-05 23:09:41 -0700

    Disable debug mode

--- syntaxforest/settings.py
+++ syntaxforest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%qj)(@kn5x#$mqpnj2iue=zlc#!zjzilpjgit&c86a+kv_s2($'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

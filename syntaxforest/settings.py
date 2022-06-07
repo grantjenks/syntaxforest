196127c1813cfa629d54621122a7260ecd3c21ba
Grant Jenks <grant.jenks@gmail.com>
2022-06-06 21:44:10 -0700

    Add DJANGO_SECRET_KEY environment variable (and change key)

--- syntaxforest/settings.py
+++ syntaxforest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import dj_database_url
+import os
 
 from pathlib import Path
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%qj)(@kn5x#$mqpnj2iue=zlc#!zjzilpjgit&c86a+kv_s2($'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'development-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
